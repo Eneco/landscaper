6125745520f8645b3e5fbe5cdee7db4f13362395
Tommy Maintz <tmaintz@schubergphilis.com>
2016-11-03 16:33:38 +0100

    Initial shell of the landscaper (golang) project

---
+++ dispatcher.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra/cobra/cmd"
+)
+
+func main() {
+	if err := cmd.RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
